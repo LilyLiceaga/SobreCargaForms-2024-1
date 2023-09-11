a3522be30c5098dfb65b0171fdb7498a70c99592
null <null>
2023-09-11 13:06:22 -0600

    Add cambios (2)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,12 +44,28 @@
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.txtbOperando23 = new System.Windows.Forms.TextBox();
+            this.btnMultiplicacion2 = new System.Windows.Forms.Button();
+            this.txtbOperando43 = new System.Windows.Forms.TextBox();
+            this.txtbOperando42 = new System.Windows.Forms.TextBox();
+            this.txtbOperando41 = new System.Windows.Forms.TextBox();
+            this.lbMultiplicacion2 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.btnMultiplicacion1 = new System.Windows.Forms.Button();
+            this.txtbOperando32 = new System.Windows.Forms.TextBox();
+            this.txtbOperando31 = new System.Windows.Forms.TextBox();
+            this.lbMultiplicacion1 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(38, 92);
+            this.label1.Location = new System.Drawing.Point(42, 92);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(34, 13);
             this.label1.TabIndex = 0;
@@ -57,7 +73,7 @@
             // 
             // txtbOperando1
             // 
-            this.txtbOperando1.Location = new System.Drawing.Point(94, 89);
+            this.txtbOperando1.Location = new System.Drawing.Point(128, 89);
             this.txtbOperando1.Name = "txtbOperando1";
             this.txtbOperando1.Size = new System.Drawing.Size(74, 20);
             this.txtbOperando1.TabIndex = 1;
@@ -68,7 +84,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(205, 89);
+            this.label2.Location = new System.Drawing.Point(239, 89);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(25, 25);
             this.label2.TabIndex = 0;
@@ -76,7 +92,7 @@
             // 
             // txtbOperando2
             // 
-            this.txtbOperando2.Location = new System.Drawing.Point(262, 89);
+            this.txtbOperando2.Location = new System.Drawing.Point(296, 89);
             this.txtbOperando2.Name = "txtbOperando2";
             this.txtbOperando2.Size = new System.Drawing.Size(74, 20);
             this.txtbOperando2.TabIndex = 1;
@@ -87,7 +103,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(391, 89);
+            this.label3.Location = new System.Drawing.Point(565, 86);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(25, 25);
             this.label3.TabIndex = 0;
@@ -96,7 +112,7 @@
             // lbSuma
             // 
             this.lbSuma.AutoSize = true;
-            this.lbSuma.Location = new System.Drawing.Point(470, 95);
+            this.lbSuma.Location = new System.Drawing.Point(643, 92);
             this.lbSuma.Name = "lbSuma";
             this.lbSuma.Size = new System.Drawing.Size(13, 13);
             this.lbSuma.TabIndex = 0;
@@ -105,9 +121,9 @@
             // btnSuma
             // 
             this.btnSuma.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSuma.Location = new System.Drawing.Point(536, 87);
+            this.btnSuma.Location = new System.Drawing.Point(708, 84);
             this.btnSuma.Name = "btnSuma";
-            this.btnSuma.Size = new System.Drawing.Size(87, 30);
+            this.btnSuma.Size = new System.Drawing.Size(110, 30);
             this.btnSuma.TabIndex = 2;
             this.btnSuma.Text = "sumar";
             this.btnSuma.UseVisualStyleBackColor = true;
@@ -116,9 +132,9 @@
             // btnSuma2
             // 
             this.btnSuma2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSuma2.Location = new System.Drawing.Point(675, 141);
+            this.btnSuma2.Location = new System.Drawing.Point(709, 141);
             this.btnSuma2.Name = "btnSuma2";
-            this.btnSuma2.Size = new System.Drawing.Size(87, 30);
+            this.btnSuma2.Size = new System.Drawing.Size(109, 30);
             this.btnSuma2.TabIndex = 9;
             this.btnSuma2.Text = "sumar";
             this.btnSuma2.UseVisualStyleBackColor = true;
@@ -126,7 +142,7 @@
             // 
             // txtbOperando22
             // 
-            this.txtbOperando22.Location = new System.Drawing.Point(262, 148);
+            this.txtbOperando22.Location = new System.Drawing.Point(296, 148);
             this.txtbOperando22.Name = "txtbOperando22";
             this.txtbOperando22.Size = new System.Drawing.Size(74, 20);
             this.txtbOperando22.TabIndex = 7;
@@ -135,7 +151,7 @@
             // 
             // txtbOperando21
             // 
-            this.txtbOperando21.Location = new System.Drawing.Point(94, 148);
+            this.txtbOperando21.Location = new System.Drawing.Point(128, 148);
             this.txtbOperando21.Name = "txtbOperando21";
             this.txtbOperando21.Size = new System.Drawing.Size(74, 20);
             this.txtbOperando21.TabIndex = 8;
@@ -145,7 +161,7 @@
             // lbSuma2
             // 
             this.lbSuma2.AutoSize = true;
-            this.lbSuma2.Location = new System.Drawing.Point(610, 151);
+            this.lbSuma2.Location = new System.Drawing.Point(644, 151);
             this.lbSuma2.Name = "lbSuma2";
             this.lbSuma2.Size = new System.Drawing.Size(13, 13);
             this.lbSuma2.TabIndex = 3;
@@ -155,7 +171,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(531, 146);
+            this.label5.Location = new System.Drawing.Point(565, 146);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(25, 25);
             this.label5.TabIndex = 4;
@@ -165,7 +181,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(205, 148);
+            this.label6.Location = new System.Drawing.Point(239, 148);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(25, 25);
             this.label6.TabIndex = 5;
@@ -174,7 +190,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(38, 151);
+            this.label7.Location = new System.Drawing.Point(42, 151);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(34, 13);
             this.label7.TabIndex = 6;
@@ -184,7 +200,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(362, 148);
+            this.label8.Location = new System.Drawing.Point(396, 148);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(25, 25);
             this.label8.TabIndex = 5;
@@ -192,18 +208,187 @@
             // 
             // txtbOperando23
             // 
-            this.txtbOperando23.Location = new System.Drawing.Point(419, 148);
+            this.txtbOperando23.Location = new System.Drawing.Point(453, 148);
             this.txtbOperando23.Name = "txtbOperando23";
             this.txtbOperando23.Size = new System.Drawing.Size(74, 20);
             this.txtbOperando23.TabIndex = 7;
             this.txtbOperando23.Text = "0";
             this.txtbOperando23.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
+            // btnMultiplicacion2
+            // 
+            this.btnMultiplicacion2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiplicacion2.Location = new System.Drawing.Point(709, 257);
+            this.btnMultiplicacion2.Name = "btnMultiplicacion2";
+            this.btnMultiplicacion2.Size = new System.Drawing.Size(109, 30);
+            this.btnMultiplicacion2.TabIndex = 25;
+            this.btnMultiplicacion2.Text = "multiplicar";
+            this.btnMultiplicacion2.UseVisualStyleBackColor = true;
+            this.btnMultiplicacion2.Click += new System.EventHandler(this.btnMultiplicacion2_Click);
+            // 
+            // txtbOperando43
+            // 
+            this.txtbOperando43.Location = new System.Drawing.Point(453, 264);
+            this.txtbOperando43.Name = "txtbOperando43";
+            this.txtbOperando43.Size = new System.Drawing.Size(74, 20);
+            this.txtbOperando43.TabIndex = 22;
+            this.txtbOperando43.Text = "0";
+            this.txtbOperando43.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtbOperando42
+            // 
+            this.txtbOperando42.Location = new System.Drawing.Point(296, 264);
+            this.txtbOperando42.Name = "txtbOperando42";
+            this.txtbOperando42.Size = new System.Drawing.Size(74, 20);
+            this.txtbOperando42.TabIndex = 23;
+            this.txtbOperando42.Text = "0";
+            this.txtbOperando42.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtbOperando41
+            // 
+            this.txtbOperando41.Location = new System.Drawing.Point(128, 264);
+            this.txtbOperando41.Name = "txtbOperando41";
+            this.txtbOperando41.Size = new System.Drawing.Size(74, 20);
+            this.txtbOperando41.TabIndex = 24;
+            this.txtbOperando41.Text = "0";
+            this.txtbOperando41.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lbMultiplicacion2
+            // 
+            this.lbMultiplicacion2.AutoSize = true;
+            this.lbMultiplicacion2.Location = new System.Drawing.Point(644, 267);
+            this.lbMultiplicacion2.Name = "lbMultiplicacion2";
+            this.lbMultiplicacion2.Size = new System.Drawing.Size(13, 13);
+            this.lbMultiplicacion2.TabIndex = 17;
+            this.lbMultiplicacion2.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(396, 262);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(24, 25);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "x";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(565, 262);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(25, 25);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "=";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(239, 261);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(24, 25);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "x";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(42, 267);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(71, 13);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "Multiplicación";
+            // 
+            // btnMultiplicacion1
+            // 
+            this.btnMultiplicacion1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiplicacion1.Location = new System.Drawing.Point(708, 200);
+            this.btnMultiplicacion1.Name = "btnMultiplicacion1";
+            this.btnMultiplicacion1.Size = new System.Drawing.Size(110, 30);
+            this.btnMultiplicacion1.TabIndex = 16;
+            this.btnMultiplicacion1.Text = "multiplicar";
+            this.btnMultiplicacion1.UseVisualStyleBackColor = true;
+            this.btnMultiplicacion1.Click += new System.EventHandler(this.btnMultiplicacion1_Click);
+            // 
+            // txtbOperando32
+            // 
+            this.txtbOperando32.Location = new System.Drawing.Point(296, 205);
+            this.txtbOperando32.Name = "txtbOperando32";
+            this.txtbOperando32.Size = new System.Drawing.Size(74, 20);
+            this.txtbOperando32.TabIndex = 14;
+            this.txtbOperando32.Text = "0";
+            this.txtbOperando32.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtbOperando31
+            // 
+            this.txtbOperando31.Location = new System.Drawing.Point(128, 205);
+            this.txtbOperando31.Name = "txtbOperando31";
+            this.txtbOperando31.Size = new System.Drawing.Size(74, 20);
+            this.txtbOperando31.TabIndex = 15;
+            this.txtbOperando31.Text = "0";
+            this.txtbOperando31.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lbMultiplicacion1
+            // 
+            this.lbMultiplicacion1.AutoSize = true;
+            this.lbMultiplicacion1.Location = new System.Drawing.Point(643, 208);
+            this.lbMultiplicacion1.Name = "lbMultiplicacion1";
+            this.lbMultiplicacion1.Size = new System.Drawing.Size(13, 13);
+            this.lbMultiplicacion1.TabIndex = 10;
+            this.lbMultiplicacion1.Text = "0";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(565, 202);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(25, 25);
+            this.label14.TabIndex = 11;
+            this.label14.Text = "=";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(239, 202);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(24, 25);
+            this.label15.TabIndex = 12;
+            this.label15.Text = "x";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(42, 208);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(71, 13);
+            this.label16.TabIndex = 13;
+            this.label16.Text = "Multiplicación";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(829, 237);
+            this.ClientSize = new System.Drawing.Size(869, 350);
+            this.Controls.Add(this.btnMultiplicacion2);
+            this.Controls.Add(this.txtbOperando43);
+            this.Controls.Add(this.txtbOperando42);
+            this.Controls.Add(this.txtbOperando41);
+            this.Controls.Add(this.lbMultiplicacion2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.btnMultiplicacion1);
+            this.Controls.Add(this.txtbOperando32);
+            this.Controls.Add(this.txtbOperando31);
+            this.Controls.Add(this.lbMultiplicacion1);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label16);
             this.Controls.Add(this.btnSuma2);
             this.Controls.Add(this.txtbOperando23);
             this.Controls.Add(this.txtbOperando22);
@@ -245,6 +430,22 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.TextBox txtbOperando23;
+        private System.Windows.Forms.Button btnMultiplicacion2;
+        private System.Windows.Forms.TextBox txtbOperando43;
+        private System.Windows.Forms.TextBox txtbOperando42;
+        private System.Windows.Forms.TextBox txtbOperando41;
+        private System.Windows.Forms.Label lbMultiplicacion2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button btnMultiplicacion1;
+        private System.Windows.Forms.TextBox txtbOperando32;
+        private System.Windows.Forms.TextBox txtbOperando31;
+        private System.Windows.Forms.Label lbMultiplicacion1;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -24,7 +24,15 @@ namespace SobreCargaForms_2024_1
         {
             lbSuma2.Text = Suma(float.Parse(txtbOperando21.Text), float.Parse(txtbOperando22.Text), float.Parse(txtbOperando23.Text)).ToString();
         }
+        private void btnMultiplicacion1_Click(object sender, EventArgs e)
+        {
+            lbMultiplicacion1.Text = Multiplicacion(float.Parse(txtbOperando31.Text), float.Parse(txtbOperando32.Text)).ToString();
+        }
 
+        private void btnMultiplicacion2_Click(object sender, EventArgs e)
+        {
+            lbMultiplicacion2.Text = Multiplicacion(float.Parse(txtbOperando41.Text), float.Parse(txtbOperando42.Text), float.Parse(txtbOperando43.Text)).ToString();
+        }
         #region Metodos Sobrecargados
         private float Suma(float sumando1, float sumando2)
         {
@@ -34,7 +42,17 @@ namespace SobreCargaForms_2024_1
         {
             return sumando1 + sumando2 + sumando3;
         }
+
+        private float Multiplicacion(float multiplicando1, float multiplicando2)
+        {
+            return multiplicando1 * multiplicando2;
+        }
+        private float Multiplicacion(float multiplicando1, float multiplicando2, float multiplicando3)
+        {
+            return multiplicando1 * multiplicando2 * multiplicando3;
+        }
         #endregion
+
 
     }
 }
