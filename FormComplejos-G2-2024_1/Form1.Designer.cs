494f753be3613a4a7ef84b3164a26f458a7ee830
Kaleb Rionda <sergiokaleb.riondamorales@gmail.com>
2023-09-13 12:56:44 -0600

    Add codigo ejercicio 7

---
+++ FormComplejos-G2-2024_1/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace FormComplejos_G2_2024_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtbReal1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtbImaginario1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbReal2 = new System.Windows.Forms.TextBox();
+            this.txtbImagnario2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbSuma = new System.Windows.Forms.Label();
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.btnConjugar = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnRestar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtbReal1
+            // 
+            this.txtbReal1.Location = new System.Drawing.Point(73, 86);
+            this.txtbReal1.Name = "txtbReal1";
+            this.txtbReal1.Size = new System.Drawing.Size(100, 20);
+            this.txtbReal1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(157, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Complejo 1";
+            // 
+            // txtbImaginario1
+            // 
+            this.txtbImaginario1.Location = new System.Drawing.Point(222, 86);
+            this.txtbImaginario1.Name = "txtbImaginario1";
+            this.txtbImaginario1.Size = new System.Drawing.Size(100, 20);
+            this.txtbImaginario1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(338, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "i";
+            // 
+            // txtbReal2
+            // 
+            this.txtbReal2.Location = new System.Drawing.Point(458, 86);
+            this.txtbReal2.Name = "txtbReal2";
+            this.txtbReal2.Size = new System.Drawing.Size(100, 20);
+            this.txtbReal2.TabIndex = 0;
+            // 
+            // txtbImagnario2
+            // 
+            this.txtbImagnario2.Location = new System.Drawing.Point(607, 86);
+            this.txtbImagnario2.Name = "txtbImagnario2";
+            this.txtbImagnario2.Size = new System.Drawing.Size(100, 20);
+            this.txtbImagnario2.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(539, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Complejo 2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(728, 86);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(12, 20);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "i";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(781, 86);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 20);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "=";
+            // 
+            // lbSuma
+            // 
+            this.lbSuma.AutoSize = true;
+            this.lbSuma.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSuma.Location = new System.Drawing.Point(831, 86);
+            this.lbSuma.Name = "lbSuma";
+            this.lbSuma.Size = new System.Drawing.Size(51, 20);
+            this.lbSuma.TabIndex = 1;
+            this.lbSuma.Text = "0 + 0 i";
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSuma.Location = new System.Drawing.Point(73, 155);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(113, 36);
+            this.btnSuma.TabIndex = 2;
+            this.btnSuma.Text = "sumar";
+            this.btnSuma.UseVisualStyleBackColor = true;
+            this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click);
+            // 
+            // btnConjugar
+            // 
+            this.btnConjugar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConjugar.Location = new System.Drawing.Point(237, 155);
+            this.btnConjugar.Name = "btnConjugar";
+            this.btnConjugar.Size = new System.Drawing.Size(113, 36);
+            this.btnConjugar.TabIndex = 2;
+            this.btnConjugar.Text = "conjugar";
+            this.btnConjugar.UseVisualStyleBackColor = true;
+            this.btnConjugar.Click += new System.EventHandler(this.btnConjugar_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiplicar.Location = new System.Drawing.Point(397, 155);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(113, 36);
+            this.btnMultiplicar.TabIndex = 2;
+            this.btnMultiplicar.Text = "multiplicar";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(189, 88);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(14, 16);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "+";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(576, 88);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(14, 16);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "+";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(394, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "+";
+            // 
+            // btnRestar
+            // 
+            this.btnRestar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestar.Location = new System.Drawing.Point(543, 155);
+            this.btnRestar.Name = "btnRestar";
+            this.btnRestar.Size = new System.Drawing.Size(113, 36);
+            this.btnRestar.TabIndex = 2;
+            this.btnRestar.Text = "restar";
+            this.btnRestar.UseVisualStyleBackColor = true;
+            this.btnRestar.Click += new System.EventHandler(this.btnRestar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(956, 251);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnConjugar);
+            this.Controls.Add(this.btnRestar);
+            this.Controls.Add(this.btnSuma);
+            this.Controls.Add(this.lbSuma);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtbImagnario2);
+            this.Controls.Add(this.txtbReal2);
+            this.Controls.Add(this.txtbImaginario1);
+            this.Controls.Add(this.txtbReal1);
+            this.Name = "Form1";
+            this.Text = "Calculadora Conjugados";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtbReal1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtbImaginario1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtbReal2;
+        private System.Windows.Forms.TextBox txtbImagnario2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbSuma;
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Button btnConjugar;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnRestar;
+    }
+}
+
---
+++ FormComplejos-G2-2024_1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FormComplejos_G2_2024_1
+{
+    public partial class Form1 : Form
+    {
+        Complejo c1;
+        Complejo c2;
+        Complejo c3;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            c1 = new Complejo(float.Parse(txtbReal1.Text), float.Parse(txtbImaginario1.Text));
+            c2 = new Complejo(float.Parse(txtbReal2.Text), float.Parse(txtbImagnario2.Text));
+
+            c3 = c1 + c2;
+            lbSuma.Text = c3.ToString();
+
+        }
+        private void btnConjugar_Click(object sender, EventArgs e)
+        {
+            lbSuma.Text = (~c3).ToString();
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            c3 = c1 * c2;
+            lbSuma.Text = c3.ToString();
+        }
+
+        private void btnRestar_Click(object sender, EventArgs e)
+        {
+            c3 = c1 - c2;
+            lbSuma.Text = c3.ToString();
+        }
+    }
+}
